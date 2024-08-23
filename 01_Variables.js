bff88d7d749ed6d4368d18296f877c93ad6dc860
Hamza Khan <hamza.khan951hk@gmail.com>
2024-08-23 19:50:29 +0500

    Variables

---
+++ 01_Variables.js
@@ -0,0 +1,11 @@
+const accountId = 234256;
+let accountEmail = "hamza.khan951hk@gmail.com";
+var accountPassword = 12345;
+
+// Var is not used now because of some errors
+
+accountCity = "Gujranwala";
+
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
