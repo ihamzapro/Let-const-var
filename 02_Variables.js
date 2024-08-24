8f0d35351bd6f13f1410ed9240dd65a0db4f9dd2
Hamza Khan <hamza.khan951hk@gmail.com>
2024-08-24 23:17:16 +0500

    Datatypes

---
+++ 02_Variables.js
@@ -0,0 +1,18 @@
+let name = "Hamza";
+let age = 18;
+let islogin = true;
+
+console.log(name);
+console.log(age);
+console.log(islogin);
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => TRUE/FALSE
+//undefined
+//Null => standalone value
+//symbol => unique
+//Object
+
+console.log(typeof null);
