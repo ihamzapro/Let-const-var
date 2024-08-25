1d6107e2d4012802bb787dc59c7c371b3372048f
Hamza Khan <hamza.khan951hk@gmail.com>
2024-08-25 15:25:55 +0500

    Conversion-Operation

---
+++ 03_conversionOperation.js
@@ -0,0 +1,26 @@
+/*let score = null;
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+/*let isLoggedIn = 1;
+let booleanLoggedIn = Boolean(isLoggedIn);
+console.log(typeof isLoggedIn);
+console.log(typeof booleanLoggedIn);
+console.log(booleanLoggedIn);*/
+
+// 1 => true; 0 => false
+// "" => false
+// "Hamza" => true
+
+let myNumber = 34;
+let myNumberString = String(myNumber);
+console.log(myNumber);
+console.log(myNumberString);
+console.log(typeof myNumberString);
